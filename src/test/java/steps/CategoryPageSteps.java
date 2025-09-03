699ff669d836943a7407a70fe3b3d3b3188b4df0
Alexander <famakiller3@gmail.com>
2025-09-03 22:12:49 +0300

    page_object

--- src/test/java/steps/CategoryPageSteps.java
+++ src/test/java/steps/CategoryPageSteps.java
@@ -2,13 +2,16 @@ package steps;
 
 import io.cucumber.java.en.Then;
 import page_objects.NavigationPageRPLSelenide;
+import page_objects.NavigationPageRplCommandName;
 
 public class CategoryPageSteps {
 
     private NavigationPageRPLSelenide navigationPageRPLSelenide = new NavigationPageRPLSelenide();
-
+    private NavigationPageRplCommandName navigationPageRplCommandName = new NavigationPageRplCommandName();
     @Then("Main page should be appear")
     public void waitUntilTextRplVisible() {navigationPageRPLSelenide.waitUntilTextRplVisible();
+        navigationPageRPLSelenide.getCommandAndScrollToThePoint();
+        navigationPageRplCommandName.getCommandToThePoint();
     }
 
 }
